0ab902b0b476812202980f922812666f6c2e580e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-11-30 19:39:08 +0200

    Implemented Role based Authentuication

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.STUDENT;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,8 +29,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index","/css/*","/js/*")
-                .permitAll()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .anyRequest()
+                .authenticated()
                 .and()
                 .httpBasic();
     }
@@ -38,10 +43,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails tyfah = User.builder()
                 .username("tyfah")
                 .password(passwordEncoder.encode( "password"))
-                .roles("STUDENT") //ROLE_STUDENT
+                .roles(STUDENT.name()) //ROLE_STUDENT
                 .build();
+        UserDetails alex = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("Password123"))
+                .roles(ADMIN.name())
+                .build();
+
         return new InMemoryUserDetailsManager(
-           tyfah
+           tyfah , alex
         );
     }
 }
